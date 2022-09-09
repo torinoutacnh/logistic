92d6e4f212c88f46c6b4f63941d7a2fd99a25964
quan <vuquan201120@gmail.com>
2022-09-09 09:31:02 +0700

    footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { Footer } from '../components/Footer';
 import styles from '../styles/Home.module.scss'
 import { Header } from './header';
 
@@ -10,12 +11,19 @@ const Home: NextPage = () => {
 
 
   return (
-    <div className={styles.container}>
-      <Header />
+    <>
 
-      <MainHome />
+      <div className={styles.container}>
 
-    </div>
+        <Header />
+
+        <MainHome />
+
+      </div>
+
+      <Footer />
+    </>
+
   )
 }
 
