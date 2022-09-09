bae4161acf24cc6fc16b354224f6f9a98f82536f
quan <vuquan201120@gmail.com>
2022-09-09 10:31:43 +0700

    Merge branch 'T' of https://github.com/torinoutacnh/logistic into Q

--- src/pages/mainHome.tsx
+++ src/pages/mainHome.tsx
@@ -1,48 +1,63 @@
-import { Input, InputAdornment, Button } from "@mui/material"
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { Button } from "@mui/material"
 import styles from '../styles/Home.module.scss'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import * as React from 'react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import TextField from '@mui/material/TextField';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 export const MainHome = () => {
+    const [value, setValue] = React.useState<Dayjs | null>(null);
+
+    const handleChange = (newValue: Dayjs | null) => {
+        setValue(newValue);
+    };
+
     return (
         <>
 
             <main className={styles.main}>
-                <div className={styles.background_booking}>
-                </div>
                 <div className={styles.booking}>
                     <h3 className={styles.booking_title}>ĐẶT XE</h3>
-                    <Input
+
+                    <p>
+                        <LocationOnIcon className={styles.booking_icon} />
+                        Điểm đi
+                    </p>
+                    <TextField
                         className={styles.booking_input}
-                        placeholder={"Điểm đến"}
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <LocationOnIcon />
-                            </InputAdornment>
-                        }
+                        id="outlined-basic"
+                        variant="outlined"
                     />
-                    <Input
+
+                    <p>
+                        <LocationOnIcon className={styles.booking_icon} />
+                        Điểm đến
+                    </p>
+                    <TextField
                         className={styles.booking_input}
-                        placeholder={"Điểm đi"}
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <LocationOnIcon />
-                            </InputAdornment>
-                        }
+                        id="outlined-basic"
+                        variant="outlined"
                     />
-                    <Input
-                        className={styles.booking_input}
-                        placeholder={"Ngày đi"}
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <CalendarMonthIcon />
-                            </InputAdornment>
-                        }
-                    />
+
+                    <p>
+                        <CalendarMonthIcon className={styles.booking_icon} />
+                        Ngày đi
+                    </p>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <MobileDatePicker
+                            className={styles.booking_input}
+                            inputFormat="DD/MM/YYYY"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+
                     <Button
                         className={styles.booking_btn}
                         variant="contained"
@@ -50,22 +65,22 @@ export const MainHome = () => {
                         endIcon={<ArrowForwardIcon style={{ fontSize: "30px" }} />}>
                         Tìm xe
                     </Button>
-                </div>
 
-                <div className={styles.controller} >
-                    <Button
-                        className={styles.booking_login}
-                        variant="contained"
-                        size="medium"
-                    >
-                        Đăng nhập
-                    </Button>
-                    <Button
-                        className={styles.booking_register}
-                        variant="outlined"
-                        size="medium" >
-                        Đăng ký
-                    </Button>
+                    <div className={styles.controller} >
+                        <Button
+                            className={styles.booking_login}
+                            variant="contained"
+                            size="medium"
+                        >
+                            Đăng nhập
+                        </Button>
+                        <Button
+                            className={styles.booking_register}
+                            variant="outlined"
+                            size="medium" >
+                            Đăng ký
+                        </Button>
+                    </div>
                 </div>
 
             </main>
