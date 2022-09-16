de236599b1322206fdab07134e04ae27f9b41482
thtien <109022683+thtien0110@users.noreply.github.com>
2022-09-16 12:36:40 +0700

    .

--- src/components/Page/Provider/MainLayout.tsx
+++ src/components/Page/Provider/MainLayout.tsx
@@ -3,9 +3,16 @@ import PageLoader from "next/dist/client/page-loader"
 import { Header } from "../Header/header"
 import { Footer } from "../Footer"
 import styles from "../../../styles/Home.module.scss"
+import { useRouter } from "next/router"
 
 
 export const MainLayout = (props: { children: React.ReactNode }) => {
+
+    const router = useRouter();
+    if (router.pathname.startsWith("/admin")) {
+        return <>{props.children}</>;
+    }
+
     return (
         <>
             <div className={styles.container}>
